88742bd9bdde0545d6161834f913eea2c570c88c
mabdelou <programming.abdu@gmail.com>
2024-01-13 14:50:09 +0100

    add onclick_funtion that let shortnavbar display when press
    Day-4 14:50 PM

--- frontend/app/PhoneNavBar/DisplaySNB/DisplaySNB.tsx
+++ frontend/app/PhoneNavBar/DisplaySNB/DisplaySNB.tsx
@@ -1,10 +1,36 @@
+'use client'
+
+
+const Display = () =>
+{
+  const DisplaySNB = document.getElementById('DisplaySNB');
+  const ShortNavBar = document.getElementById('ShortNavBar');
+
+  if(DisplaySNB && ShortNavBar)
+  {
+    if(ShortNavBar.style.display == "block")
+    {
+      ShortNavBar.style.display = "none";
+      DisplaySNB.style.right = "0px";
+    }
+    else
+    {
+      ShortNavBar.style.display = "block";
+      DisplaySNB.style.right = "75px";
+    }
+  }
+
+
+}
 
 
 export default function DisplaySNB() {
     return (
-      <div className="fixed sm:hidden top-[10px] w-[25px] right-[0px] h-[25px] bg-[#FC033D] rounded-md z-40">
-         
-      </div>
-    )
+      <button
+        id="DisplaySNB"
+        className="fixed sm:hidden top-[10px] w-[25px] right-[0px] h-[25px] bg-[#FC033D] rounded-md z-40"
+        onClick={()=>{Display();}}>
+      </button>
+    );
   }
   
--- frontend/app/PhoneNavBar/PhoneNavBar.tsx
+++ frontend/app/PhoneNavBar/PhoneNavBar.tsx
@@ -5,7 +5,7 @@ export default function PhoneNavBar() {
     return (
       <div>
         <DisplaySNB/>
-        <div className="fixed sm:hidden w-[100px] h-[100vh] bg-[#CCCCCC] top-[0px] ml-[calc(100vw-100px)] z-30">
+        <div id="ShortNavBar" className="fixed hidden sm:hidden w-[100px] h-[100vh] bg-[#CCCCCC] top-[0px] ml-[calc(100vw-100px)] z-30">
           <ShortNavBar/>
         </div>
       </div>
