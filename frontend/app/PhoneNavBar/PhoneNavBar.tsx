917aa3ae00874d56f68c6f0c9f684be3bd7a8b8d
mabdelou <programming.abdu@gmail.com>
2024-01-13 16:14:24 +0100

    small chnage to fix small error, need more work
    Day-4 16:14 P,

--- frontend/app/PhoneNavBar/PhoneNavBar.tsx
+++ frontend/app/PhoneNavBar/PhoneNavBar.tsx
@@ -1,11 +1,30 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
 import DisplaySNB from "./DisplaySNB/DisplaySNB";
 import ShortNavBar from "./ShortNavBar/ShortNavBar";
 
 export default function PhoneNavBar() {
-    return (
+  // const [NumberOfRenders, setNumberOfRenders] = useState<number>(0);
+  useEffect(()=>
+  {
+    const interval = setInterval(()=>{
+      const ShortNavBar = document.getElementById('ShortNavBar');
+      const DisplaySNB = document.getElementById('DisplaySNB');
+      if(window.innerWidth >= 640 && ShortNavBar && DisplaySNB)
+        ShortNavBar.style.display = "none";
+        // DisplaySNB.style.right = "25px";
+    },1000);
+    return ()=>
+    {
+      clearInterval(interval);
+    }
+  },[]);
+  return (
       <div>
         <DisplaySNB/>
-        <div id="ShortNavBar" className="fixed hidden sm:hidden w-[100px] h-[100vh] bg-[#CCCCCC] top-[0px] ml-[calc(100vw-100px)] z-30">
+        <div id="ShortNavBar" className="fixed hidden w-[100px] h-[100vh] bg-[#CCCCCC] top-[0px] ml-[calc(100vw-100px)] z-30">
           <ShortNavBar/>
         </div>
       </div>
