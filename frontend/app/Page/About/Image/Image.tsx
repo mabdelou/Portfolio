ea1ca907cb6c9ea6664349a4d20d6a6090e94282
mabdelou <programming.abdu@gmail.com>
2024-01-14 16:56:09 +0100

    small update
    Day-5 16:56 PM

--- frontend/app/Page/About/Image/Image.tsx
+++ frontend/app/Page/About/Image/Image.tsx
@@ -2,11 +2,11 @@
 
 export default function Image() {
   return (
-    <div className="relative scale-[1] w-[50vw] h-[750px]">
+    <div className="relative hidden sm:block scale-[1] w-[50vw] h-[750px]">
       <img
         src="/photographII.png"
         alt="photograph"
-        className="relative z-10 w-[500px] sm:h-[300px] md:h-[400px] lg:h-[500px]"/>
+        className="relative z-10 sm:h-[300px] md:h-[400px] lg:h-[500px] sm:w-[300px] md:w-[400px] lg:w-[500px] sm:mt-[calc(100vh-350px)] md:mt-[calc(100vh-450px)] lg:mt-[calc(100vh-550px)]"/>
     </div>
   )
 }
