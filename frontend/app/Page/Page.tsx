aee87acd3d58483ba36c58a19f7e791685766fee
mabdelou <programming.abdu@gmail.com>
2024-01-14 23:06:14 +0100

    add scrolling animation
    Day-5 23:06 PM

--- frontend/app/Page/Page.tsx
+++ frontend/app/Page/Page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Home from "./Home/Home";
 import About from "./About/About";
 import Service from "./Service/Service";
@@ -5,11 +7,36 @@ import Resume from "./Resume/Resume";
 import Blog from "./Blog/Blog";
 import Content from "./Content/Content";
 import Portfolio from "./Portfolio/Portfolio";
+import { useEffect, useState } from 'react';
+import '/app/PhoneNavBar/Style/Style.css'
+
 
 
 export default function Page() {
+  useEffect(()=>
+  {
+    const Parrent =  document.getElementById('Parrent');
+    if(Parrent)
+    {
+      Parrent.addEventListener('wheel', handleScroll);
+    }
+    function handleScroll(event: WheelEvent) {
+      const deltaY = event.deltaY;
+
+      if (deltaY > 0 && Parrent)
+      {
+        Parrent.style.animation = "Scroll_down 3s 1 forwards";
+        console.log("down");
+      }
+      else if (deltaY < 0 && Parrent)
+      {
+        Parrent.style.animation = "Scroll_up 3s 1 forwards";
+        console.log("up");
+      }  
+    }
+  },[]);
   return (
-    <div className="relative w-[100vw] h-[calc(750px*7)]">
+    <div id="Parrent" className="relative w-[100vw] h-[calc(750px*7)]">
         <About/>
         <Home/>
         <Service/>
