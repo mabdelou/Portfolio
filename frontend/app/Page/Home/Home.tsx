4198248d22bba6134683ccef88cc7d6b177278a7
mabdelou <programming.abdu@gmail.com>
2024-01-16 16:44:58 +0100

    addd home part
    Day-7 16:44 PM

--- frontend/app/Page/Home/Home.tsx
+++ frontend/app/Page/Home/Home.tsx
@@ -3,8 +3,27 @@
 export default function Home() {
   return (
     <div className="relative ">
-      <div className="relative w-[100vw] h-[100vh] bg-[#666666]">
-        
+      <div className="relative flex w-[300vw] h-[100vh] bg-[#E5ECF1]">
+        <div className="relative flex items-center justify-center  w-[100vw] h-[100vh]">
+          <p className="text-center  text-[300px]  font-black">
+            DESIGNER
+          </p>
+        </div>
+        <div className="relative w-[100vw] h-[100vh] text-center text-[300px] font-black">
+          <p className="text-center  text-[300px]  font-black">
+            DEVELOPER
+          </p>
+        </div>
+        <div className="relative w-[100vw] h-[100vh] text-center text-[300px] font-black">
+          <p className="text-center  text-[300px]  font-black">
+            PROGRAMMER
+          </p>
+        </div>
+      </div>
+      <div className="absolute flex items-center justify-center space-x-[50px] left-1/2 -translate-x-1/2 bottom-[5vh] w-[250px] h-[75px]">
+        <div className="relative w-[20px] h-[20px] bg-[#FC033D] rounded-3xl"></div>
+        <div className="relative w-[20px] h-[20px] bg-[#666666] rounded-3xl"></div>
+        <div className="relative w-[20px] h-[20px] bg-[#666666] rounded-3xl"></div>
       </div>
     </div>
   )
