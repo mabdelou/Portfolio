d81c751db795a64681fb26f10d0971695520ee13
mabdelou <programming.abdu@gmail.com>
2024-01-21 17:09:03 +0100

    version O1

--- frontend/app/Page/Contact/Contact.tsx
+++ frontend/app/Page/Contact/Contact.tsx
@@ -3,9 +3,10 @@
 export default function Contact() {
   return (
     <div className="relative ">
-      <div className="relative w-[100vw] h-[100vh] bg-[#E5ECF1]">
+      <div className="relative w-[100vw] h-[100vh] bg-[#E5ECF1] ">
         
       </div>
+      
     </div>
   )
 }
