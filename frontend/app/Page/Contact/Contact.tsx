64ff43c2d8b0e99010e5e2aba54c5b77b4658671
mabdelou <programming.abdu@gmail.com>
2024-01-30 11:06:43 +0100

    checkpoint (backup)

--- frontend/app/Page/Contact/Contact.tsx
+++ frontend/app/Page/Contact/Contact.tsx
@@ -5,20 +5,20 @@ export default function Contact() {
     <div className="relative ">
       <div className="relative w-[100vw] h-[calc(100vh-50px)] mt-[50px] bg-[#E5ECF1] ">
         <div className="relative w-[100vw] h-[100px] ">
-          <p className="relative top-1/2 transform -translate-y-1/2 text-center text-[#FC033D] text-[45px] font-black">
+          <p className="relative top-1/2 transform -translate-y-1/2 text-center text-[#FC033D]  sm:text-[15px] md:text-[30px] lg:text-[45px] font-black">
               LET YOUR IMAGINATION BE TRUTH BY ONE CLICK 
           </p>
         </div>
         <div className="relative flex w-[100vw] h-[200px] ">
         <div className="relative w-[50vw] h-[200px]">
-            <div className="relative w-[400px] h-[100px] mx-auto mt-[50px] ">
+            <div className="relative w-[100%] h-[100px] mx-auto mt-[50px] ">
               <img className="relative w-[42px] h-[42px] mx-auto" src="/logo/phone-128.png" />
               <div className="relative mx-auto w-[75px] h-[35px]"> 
                 <p className="text-center text-[#666666] text-[25px] font-black">
                   Phone
                 </p>
               </div>
-              <div className="relative mx-auto w-[225px] h-[25px]">
+              <div className="relative mx-auto w-[100%] h-[25px]">
                 <p className="text-center text-[#FC033D] font-bold">
                   +212616170907
                 </p>
@@ -26,14 +26,14 @@ export default function Contact() {
             </div>
           </div>
           <div className="relative w-[50vw] h-[200px]">
-            <div className="relative w-[400px] h-[100px] mx-auto mt-[50px] ">
+            <div className="relative w-[100%] h-[100px] mx-auto mt-[50px] ">
               <img className="relative w-[42px] h-[42px] mx-auto" src="/logo/email-128.png" />
               <div className="relative mx-auto w-[75px] h-[35px]"> 
                 <p className="text-center text-[#666666] text-[25px] font-black">
                   Email
                 </p>
               </div>
-              <div className="relative mx-auto w-[225px] h-[25px]">
+              <div className="relative mx-auto w-[100%] h-[25px]">
                 <p className="text-center text-[#FC033D] font-bold">
                   programming.abdu@gmail.com
                 </p>
@@ -43,16 +43,16 @@ export default function Contact() {
         </div>
         <div className="relative  w-[calc(100vw-20px)] h-[calc((100vh-50px)-320px)] mt-[10px] mx-auto bg-[#d3d9de] rounded-lg">
           <div className="relative flex w-[100%] h-[75%] top-[10%]">
-            <div className="relative w-[50%] h-[100%]">
-              <p className="ml-[65px] mt-[20px]">
+            <div className="relative w-[50%] h-[100%] space-y-2 ">
+              <p className="ml-[65px]">
                 First Name:
               </p>
               <div className="relative bg-[#E5ECF1] w-[90%] h-[35px] ml-[50px] rounded-md"></div>
-              <p className="ml-[65px] mt-[40px]">
+              <p className="ml-[65px]">
                 Your Email:
               </p>
               <div className="relative bg-[#E5ECF1] w-[90%] h-[35px] ml-[50px] rounded-md"></div>
-              <p className="ml-[65px] mt-[40px]">
+              <p className="ml-[65px]">
                 Your Subject:
               </p >
               <div className="relative bg-[#E5ECF1] w-[90%] h-[35px] ml-[50px] rounded-md"></div>
